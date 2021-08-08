5f64402ff0d02c182459824a0a33c50ec7e51b22
sricherry6 <priyanka.chandrabhatla@gmail.com>
2021-08-08 11:15:13 +0530

    Updation

--- ml_utils.py
+++ ml_utils.py
@@ -1,9 +1,13 @@
-
+from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import pandas as pd
+from pandas import read_csv
+import h2o
+from h2o.automl import H2OAutoML
+import subprocess
 
 # Defining classification model 
 clf = RandomForestClassifier(n_estimators=100, random_state=0)
@@ -15,17 +19,27 @@ classes = {
     False : 'No defects'
 }
 r_classes = {y: x for x, y in classes.items()}
-# function to load_model at the start
+
+# function to train and load the model during startup
 def load_model():
-        data = pd.read_csv('bug_pred.csv')
-        X = data.drop('defects', axis = 1)
-        y = data['defects']
+    # load the dataset from the official sklearn datasets
+    # X, y = datasets.load_iris(return_X_y=True)
+    df=read_csv("dataset/bug_pred.csv", sep=",")
+    X = df.drop("defects", axis=1)
+    y = df["defects"]
 
-        X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
-        clf.fit(X_train, y_train)
+# do the test-train split and train the model
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
+    clf.fit(X_train, y_train)
 
-        acc = accuracy_score(y_test, clf.predict(X_test))
-        print(f"Model trained with accuracy: {round(acc, 3)}")
+# calculate the print the accuracy score
+    acc = accuracy_score(y_test, clf.predict(X_test))
+    print(f"Model trained with accuracy: {round(acc, 3)}")
+
+#Generating Explainability File
+    subprocess.call(["jupyter","nbconvert","--to","notebook","--inplace","--execute","dataset/explainable_AI_starter.ipynb"])
+    subprocess.call(["jupyter","nbconvert","dataset/explainable_AI_starter.ipynb","--no-input","--to","html"])
+    print("Explainability file generated")
 
 def predict(query_data):
     x = list(query_data.dict().values())
